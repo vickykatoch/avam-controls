79d244a8271263707492c41f30d71452f38a1c63
Balwinder Katoch <balwinderkatoch@MacBook-Pro.local>
2018-02-23 18:16:12 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DropDownComponent } from './drop-down/drop-down.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropDownComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/drop-down/drop-down.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'avam-drop-down',
+  templateUrl: './drop-down.component.html',
+  styleUrls: ['./drop-down.component.scss']
+})
+export class DropDownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
